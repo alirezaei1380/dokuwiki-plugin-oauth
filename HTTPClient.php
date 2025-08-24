adbe58a5ac7d0400655893084e478be41fd4fcbb
alirezaei1380 <59190480+alirezaei1380@users.noreply.github.com>
2025-08-24 18:09:29 +0330

    Update HTTPClient.php

--- HTTPClient.php
+++ HTTPClient.php
@@ -6,6 +6,14 @@ use dokuwiki\HTTP\DokuHTTPClient;
 use OAuth\Common\Http\Client\ClientInterface;
 use OAuth\Common\Http\Uri\UriInterface;
 
+function endsWith( $haystack, $needle ) {
+    $length = strlen( $needle );
+    if( !$length ) {
+      return true;
+    }
+    return substr( $haystack, -$length ) === $needle;
+}
+
 /**
  * Implements the client interface using DokuWiki's HTTPClient
  */
@@ -19,20 +27,23 @@ class HTTPClient implements ClientInterface
         $method = 'POST'
     ) {
         $http = new DokuHTTPClient();
-        $http->keep_alive = false;
-        $http->headers = array_merge($http->headers, $extraHeaders);
-
-        $ok = $http->sendRequest($endpoint->getAbsoluteUri(), $requestBody, $method);
-        if (!$ok || $http->status < 200 || $http->status > 299) {
-            $msg = "An error occured during the request to the oauth provider:\n";
-            throw new HttpTokenResponseException(
-                $msg . $http->error . ' [HTTP ' . $http->status . ']',
-                $http->status,
-                $http->error,
-                $http->resp_body
-            );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $endpoint->getAbsoluteUri());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            if (endsWith($endpoint->getAbsoluteUri(), '/connect/token')) {
+                $requestBody['grant_type'] = 'client_credentials';
+                $extraHeaders[] = 'Content-Type: application/x-www-form-urlencoded';
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($requestBody, "", "&", PHP_QUERY_RFC3986));
         }
-
-        return $http->resp_body;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array_merge($http->headers, $extraHeaders));
+        $response = curl_exec($ch);
+        if ($response === true || $response === false) {
+            return "";
+        } else {
+            return $response;
+        }
     }
 }
